4bbdead75905052fa3207adfd2bc482ef8bb3e45
Gianni Ceccarelli <gianni.ceccarelli@broadbean.com>
2019-07-18 16:17:03 +0100

    Dzil-build release 3.0 (from 67289b6 on master)

--- lib/Net/Stomp/MooseHelpers.pm
+++ lib/Net/Stomp/MooseHelpers.pm
@@ -1,6 +1,27 @@
 package Net::Stomp::MooseHelpers;
+$Net::Stomp::MooseHelpers::VERSION = '3.0';
+{
+  $Net::Stomp::MooseHelpers::DIST = 'Net-Stomp-MooseHelpers';
+}
 
 # ABSTRACT: set of helper roles and types to deal with Net::Stomp
+
+
+1;
+
+__END__
+
+=pod
+
+=encoding UTF-8
+
+=head1 NAME
+
+Net::Stomp::MooseHelpers - set of helper roles and types to deal with Net::Stomp
+
+=head1 VERSION
+
+version 3.0
 
 =head1 DESCRIPTION
 
@@ -18,6 +39,15 @@ read back the dumped frames.
 We also provide some types (L<Net::Stomp::MooseHelpers::Types>) and
 exception classes (L<Net::Stomp::MooseHelpers::Exceptions>).
 
-=cut
+=head1 AUTHOR
 
-1;
+Gianni Ceccarelli <gianni.ceccarelli@net-a-porter.com>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2014 by Net-a-porter.com.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=cut
